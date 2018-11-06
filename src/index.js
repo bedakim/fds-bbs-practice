b2039a7bae9eca35ffd59001246305a3a6189181
Kim Minyeob <stankim0525@gmail.com>
2018-11-06 17:11:59 +0900

    newpost

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import '@babel/polyfill'
 import axios from 'axios'
 
 const api = axios.create({
+  //바깥에서 주입해준 환경변수를 사용하는 코드
+  // 이 컴퓨터에서만 사용할 환경변수를 설정하기 위해서 .env 파일을 복사한다
   baseURL: process.env.API_URL
 })
 
@@ -68,9 +70,35 @@ async function drawPostList() {
   const frag = document.importNode(templates.postList, true)
 
   // 2. 요소 선택
+  const listEl = frag.querySelector('.post-list')
+  const createEl = frag.querySelector('.create')
   // 3. 필요한 데이터 불러오기
+  const {data: postList} = await api.get('/posts?_expand=user')
+
+
   // 4. 내용 채우기
+  for (const postItem of postList){
+    const frag = document.importNode(templates.postItem, true)
+
+    const idEl = frag.querySelector('.id')
+    const titleEl = frag.querySelector('.title')
+    const authorEl = frag.querySelector('.author')
+
+    idEl.textContent = postItem.id
+    titleEl.textContent = postItem.title
+    authorEl.textContent = postItem.user.username
+
+    titleEl.addEventListener('click', e=>{
+      drawPostDetail(postItem.id)
+    })
+
+    listEl.appendChild(frag)
+  }
+
   // 5. 이벤트 리스너 등록하기
+  createEl.addEventListener('click', e => {
+    drawNewPostForm()
+  })
 
   // 6. 템플릿을 문서에 삽입
   rootEl.textContent = ''
@@ -79,20 +107,103 @@ async function drawPostList() {
 
 async function drawPostDetail(postId) {
   // 1. 템플릿 복사
+  const frag = document.importNode(templates.postDetail, true)
   // 2. 요소 선택
+  // const detailEl = frag.querySelector('.post-detail')
+  const titleEl = frag.querySelector('.title')
+  const authorEl = frag.querySelector('.author')
+  const bodyEl = frag.querySelector('.body')
+  const backEl = frag.querySelector('.back')
+  const commentListEl = frag.querySelector('.comment-list')
+  const commentFormEl = frag.querySelector('.comment-form')
   // 3. 필요한 데이터 불러오기
+  const {data: {title, body, user, comments}} = await api.get('/posts/' + postId, {
+    params:{
+      _expand: 'user',
+      _embed: 'comments'
+    }
+  })
+  const params = new URLSearchParams()
+
+  comments.forEach(c => {
+    params.append('id', c.userId)
+  });
+  const {data: userList} = await api.get('/users', {
+    params
+  })
+
   // 4. 내용 채우기
+
+  titleEl.textContent = title
+  bodyEl.textContent = body
+  authorEl.textContent = user.username
+  for (const commentItem of comments){
+    // 1. 템플릿 복사
+    const frag = document.importNode(templates.commentItem, true)
+  // 2. 요소 선택
+  const authorEl = frag.querySelector('.author')
+  const bodyEl = frag.querySelector('.body')
+  const deleteEl = frag.querySelector('.delete')
+
+  // 3. 필요한 데이터 불러오기-필요없음
+  // 4. 내용 채우기
+  bodyEl.textContent = commentItem.body
+
+  const user = userList.find(item => item.id === commentItem.userId)
+  authorEl.textContent = user.username
+
   // 5. 이벤트 리스너 등록하기
   // 6. 템플릿을 문서에 삽입
+  commentListEl.appendChild(frag)
+  }
+
+
+  // 5. 이벤트 리스너 등록하기
+  backEl.addEventListener('click', e=>{
+    drawPostList()
+  })
+
+  commentFormEl.addEventListener('submit', async e => {
+    e.preventDefault()
+    const body = e.target.elements.body.value
+    await api.post(`/posts/${postId}/comments`, {
+      body
+    })
+    drawPostDetail(postId)
+  })
+
+  // 6. 템플릿을 문서에 삽입.
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
 }
 
 async function drawNewPostForm() {
   // 1. 템플릿 복사
+  const frag = document.importNode(templates.postForm, true)
   // 2. 요소 선택
+const formEl = frag.querySelector('.post-form')
+const backEl = frag.querySelector('.back')
   // 3. 필요한 데이터 불러오기
   // 4. 내용 채우기
   // 5. 이벤트 리스너 등록하기
+  formEl.addEventListener('submit', async e =>{
+    e.preventDefault()
+    const title = e.target.elements.title.value
+    const body = e.target.elements.body.value
+    await api.post('/posts', {
+      title,
+      body
+    })
+    drawPostList()
+  })
+  backEl.addEventListener('click', e=>{
+    e.preventDefault()
+    drawPostList()
+  })
+
   // 6. 템플릿을 문서에 삽입
+  rootEl.textContent = ''
+  rootEl.appendChild(frag)
 }
 
 async function drawEditPostForm(postId) {
